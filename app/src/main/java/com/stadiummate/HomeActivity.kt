30938f6ca028b7ea4b968f6f1214c680188903f1
Mayank <mayankkumarsohanda@gmail.com>
2023-03-03 01:41:47 +0530

    Added auth

--- app/src/main/java/com/stadiummate/HomeActivity.kt
+++ app/src/main/java/com/stadiummate/HomeActivity.kt
@@ -84,7 +84,6 @@ fun MatchCardOne(
                     painterResource(id = R.mipmap.ic_aus),
                     contentDescription = null,
                     contentScale = ContentScale.Crop,
-
                     modifier = Modifier.size(56.dp)
                 )
                 Text(
